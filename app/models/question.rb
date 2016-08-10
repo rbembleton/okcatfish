a62706a3e2d4d8c0a4fcc28c3c46421cbf1e2e89
RB Embleton <rbembleton@aol.com>
2016-08-10 12:40:30 -0400

    Delete old code

--- app/models/question.rb
+++ app/models/question.rb
@@ -24,27 +24,12 @@ class Question < ActiveRecord::Base
       # Question
       #   .joins('JOIN answers ON answers.question_id = questions.id LEFT OUTER JOIN user_responses ON user_responses.answer_id = answers.id')
       #   .group('questions.id')
-      #
-      #
-      #   # .having('COUNT(user_responses.user_id) = 0')
-      #
-      #   # .where('user_responses.user_id = ? OR user_responses.user_id IS NULL', user_id)
-      #
-      #
-      #   # .order(order: :asc)
-      #   # .limit(20)
+      #   .having('COUNT(user_responses.user_id) = 0')
+      #   .where('user_responses.user_id = ? OR user_responses.user_id IS NULL', user_id)
+      #   .order(order: :asc)
+      #   .limit(20)
 
       Question.where('id NOT IN (?)', (User.find(user_id).questions_answered.select(:id))).order(order: :asc).limit(20)
-
-
-      # Question
-      #   .joins(:answers)
-      #   .joins('LEFT OUTER JOIN user_responses ON user_responses.answer_id = answers.id')
-      #   .group('questions.id')
-      #   .having('user_responses.user_id != ?', user_id)
-        # .having("COUNT((user_responses.user_id = ?) = 0)", user_id)
-
-        # .where('user_responses.user_id = ? OR user_responses.user_id IS NULL', user_id)
 
     else
       Question.order(order: :asc).limit(20)
