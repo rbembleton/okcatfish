c6bc70735b84f5d84dd1f61b491b08d59dfd205f
RB Embleton <rbembleton@aol.com>
2016-08-15 19:01:25 -0400

    Fix webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,44 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/application.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ],
+    noParse: /node_modules\/quill\/dist/
+  },
+  devtool: 'source-maps'
+};
